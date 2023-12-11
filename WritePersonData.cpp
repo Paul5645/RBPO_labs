738851ff379ed0af8ddc05148f53693b57ddc62b
Paul5645 <120225770+Paul5645@users.noreply.github.com>
2023-12-11 23:47:39 +0300

    Delete WritePersonData.cpp

--- WritePersonData.cpp
+++
@@ -1,9 +0,0 @@
-#include "header.h"
-
-void WritePersonData(const string& name, const string& weight, const string& height, const unsigned short* age, const double salary) {
-	cout << "Name: " << name << endl;
-	cout << "Weight: " << weight << endl;
-	cout << "Height: " << height << endl;
-	cout << "Age: " << *age << endl;
-	cout << "Salary: " << salary << endl;
-}
